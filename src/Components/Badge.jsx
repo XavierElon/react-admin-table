eb6be2fa9775c26d4b1103e21c83f9596a47f8c4
XavierElon1 <xavierh93@hotmail.com>
2021-05-19 18:00:28 -0400

    add badge component

--- src/Components/Badge.jsx
+++ src/Components/Badge.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import styled from "styled-components";
 
-export const blueBadge = styled('div')`
-    width: 114px;
-    height: 28px;
+export const BlueBadge = styled('div')`
+    width: 128px;
+    height: 30px;
     border-radius: 3px;
-    fill: #3d7aa9;
+    background: #3d7aa9;
+    color: white;
+    margin-left: 20px;
+    padding: 2px;
+`
+export const GreenBadge = styled('div')`
+    width: 128px;
+    height: 30px;
+    border-radius: 3px;
+    background: #5e8000;
+    color: white;
+    margin-left: 20px;
+    padding: 2px;
 `
 
+export const RedBadge = styled('div')`
+    width: 128px;
+    height: 30px;
+    border-radius: 3px;
+    background: #eb1316;
+    color: white;
+    margin-left: 20px;
+    padding: 2px;
+`
+export const GreyBadge = styled('div')`
+    width: 128px;
+    height: 30px;
+    border-radius: 3px;
+    background: #525051;
+    color: white;
+    margin-left: 20px;
+    padding: 2px;
+`
