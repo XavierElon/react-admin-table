26d0063700f1c1eb6eea4850829b4dec1b1bc873
XavierElon1 <xavierh93@hotmail.com>
2021-06-17 11:53:09 -0400

    update request form styling

--- src/pages/DenyDetails.jsx
+++ src/pages/DenyDetails.jsx
@@ -82,7 +82,7 @@ const submitStyle = {
   width: "112px",
   height: "36px",
   borderRadius: "4px",
-  backgroundColor: "#700017",
+  backgroundColor: "#1672B5",
   color: "white",
   fontSize: "14px",
   lineHeight: "32px",
